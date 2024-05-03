f806d9d2d70463010c2f6eb2106c2091a4642330
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-05-03 23:26:40 +0600

    add roles card template and other

--- src/pages/dashboard/RolesAndUsers/Roles/CreateRoles/CreateRolesTemplate.tsx
+++ src/pages/dashboard/RolesAndUsers/Roles/CreateRoles/CreateRolesTemplate.tsx
@@ -1,8 +1,192 @@
+import { Button, Card } from "react-bootstrap";
 
 const CreateRolesTemplate = () => {
     return (
         <>
-            
+            <Card>
+                <Card.Body>
+                    <div className="d-flex align-items-center justify-content-between mb-3">
+                        <div>
+                            <span className="fs-5 fw-semibold">Tests</span>
+                        </div>
+                        <div>
+                            <Button variant="primary" className="btn-sm me-2">
+                                <i className="bi bi-check-lg"></i>
+                            </Button>
+                            <Button variant="danger" className="btn-sm">
+                                <i className="bi bi-x-lg"></i>
+                            </Button>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Tests</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Create</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Edit</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Delete</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <div className="d-flex align-items-center justify-content-between mb-3">
+                        <div>
+                            <span className="fs-5 fw-semibold">Tests</span>
+                        </div>
+                        <div>
+                            <Button variant="primary" className="btn-sm me-2">
+                                <i className="bi bi-check-lg"></i>
+                            </Button>
+                            <Button variant="danger" className="btn-sm">
+                                <i className="bi bi-x-lg"></i>
+                            </Button>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Tests</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Create</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Edit</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Delete</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <div className="d-flex align-items-center justify-content-between mb-3">
+                        <div>
+                            <span className="fs-5 fw-semibold">Tests</span>
+                        </div>
+                        <div>
+                            <Button variant="primary" className="btn-sm me-2">
+                                <i className="bi bi-check-lg"></i>
+                            </Button>
+                            <Button variant="danger" className="btn-sm">
+                                <i className="bi bi-x-lg"></i>
+                            </Button>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Tests</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Create</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Edit</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Delete</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <div className="d-flex align-items-center justify-content-between mb-3">
+                        <div>
+                            <span className="fs-5 fw-semibold">Tests</span>
+                        </div>
+                        <div>
+                            <Button variant="primary" className="btn-sm me-2">
+                                <i className="bi bi-check-lg"></i>
+                            </Button>
+                            <Button variant="danger" className="btn-sm">
+                                <i className="bi bi-x-lg"></i>
+                            </Button>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Tests</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Create</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between mb-1">
+                        <h5 className="fs-5 fw-semibold">Edit</h5>
+                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h5 className="fs-5 fw-semibold">Delete</h5>
+                        <div className="form-check form-switch" style={{ width: "20%" }}>
+                            <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
+                            <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
         </>
     );
 };
--- src/pages/dashboard/RolesAndUsers/Roles/CreateRoles/index.tsx
+++ src/pages/dashboard/RolesAndUsers/Roles/CreateRoles/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, Col, Form, InputGroup, Row } from "react-bootstrap";
 import PageTitle from "../../../../../components/PageTitle";
+import CreateRolesTemplate from "./CreateRolesTemplate";
 
 const CreateRolesDashboard = () => {
     return (
@@ -47,6 +48,7 @@ const CreateRolesDashboard = () => {
                     </div>
                     <Row>
                         <Col lg={4} md={6} sm={12}>
+                            <CreateRolesTemplate />
                             <Card>
                                 <Card.Body>
                                     <div className="d-flex align-items-center justify-content-between mb-3">
@@ -62,75 +64,44 @@ const CreateRolesDashboard = () => {
                                             </Button>
                                         </div>
                                     </div>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card>
-                                <Card.Body>
-                                    <div className="d-flex align-items-center justify-content-between mb-3">
-                                        <div>
-                                            <span className="fs-5 fw-semibold">Tests</span>
-                                        </div>
-                                        <div>
-                                            <Button variant="primary" className="btn-sm me-2">
-                                                <i className="bi bi-check-lg"></i>
-                                            </Button>
-                                            <Button variant="danger" className="btn-sm">
-                                                <i className="bi bi-x-lg"></i>
-                                            </Button>
-                                        </div>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card>
-                                <Card.Body>
-                                    <div className="d-flex align-items-center justify-content-between mb-3">
-                                        <div>
-                                            <span className="fs-5 fw-semibold">Tests</span>
-                                        </div>
-                                        <div>
-                                            <Button variant="primary" className="btn-sm me-2">
-                                                <i className="bi bi-check-lg"></i>
-                                            </Button>
-                                            <Button variant="danger" className="btn-sm">
-                                                <i className="bi bi-x-lg"></i>
-                                            </Button>
-                                        </div>
-                                    </div>
-                                   <hr />
+                                    <hr />
                                     <div className="d-flex align-items-center justify-content-between mb-1">
-                                            <h5 className="fs-5 fw-semibold">Tests</h5>
-                                        <div className="form-check form-switch m-0" style={{ width: "20%"}}>
+                                        <h5 className="fs-5 fw-semibold">Tests</h5>
+                                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
                                             <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
                                             <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
                                         </div>
                                     </div>
                                     <div className="d-flex align-items-center justify-content-between">
-                                            <h5 className="fs-5 fw-semibold">Create</h5>
-                                        <div className="form-check form-switch" style={{ width: "20%"}}>
+                                        <h5 className="fs-5 fw-semibold">Create</h5>
+                                        <div className="form-check form-switch" style={{ width: "20%" }}>
                                             <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
                                             <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
                                         </div>
                                     </div>
                                     <div className="d-flex align-items-center justify-content-between mb-1">
-                                            <h5 className="fs-5 fw-semibold">Edit</h5>
-                                        <div className="form-check form-switch m-0" style={{ width: "20%"}}>
+                                        <h5 className="fs-5 fw-semibold">Edit</h5>
+                                        <div className="form-check form-switch m-0" style={{ width: "20%" }}>
                                             <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
                                             <label className="form-check-label w-100 d-inline " htmlFor="flexSwitchCheckDefault"></label>
                                         </div>
                                     </div>
                                     <div className="d-flex align-items-center justify-content-between">
-                                            <h5 className="fs-5 fw-semibold">Delete</h5>
-                                        <div className="form-check form-switch" style={{ width: "20%"}}>
+                                        <h5 className="fs-5 fw-semibold">Delete</h5>
+                                        <div className="form-check form-switch" style={{ width: "20%" }}>
                                             <input className="form-check-input m-0 w-100 " type="checkbox" role="switch" style={{ padding: "15px 0px" }} id="flexSwitchCheckDefault" />
                                             <label className="form-check-label w-100 d-inline" htmlFor="flexSwitchCheckDefault"></label>
                                         </div>
                                     </div>
                                 </Card.Body>
                             </Card>
+                        </Col>
+                        <Col lg={4} md={6} sm={12}>
+                            <CreateRolesTemplate />
+                        </Col>
+                        <Col lg={4} md={6} sm={12}>
+                            <CreateRolesTemplate />
+
                         </Col>
                     </Row>
 
