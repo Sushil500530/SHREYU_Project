0f656cccee807d8ea52248e2671b4ddaf7debe75
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-05-03 16:06:20 +0600

    add create button show modal

--- src/pages/dashboard/Contracts/index.tsx
+++ src/pages/dashboard/Contracts/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
 // components
-import { Button, Card, Col, Row, } from "react-bootstrap";
+import { Button, Card, Col, Dropdown, Row, ButtonGroup } from "react-bootstrap";
 import PageTitle from '../../../components/PageTitle';
 import ContractsTable from './ContractsTable';
 import MidItemBar from './MidItemBar';
+import FeatherIcon from 'feather-icons-react';
 
 
 
@@ -45,9 +46,52 @@ const ContractsDashboard = () => {
                             </div>
                             <div className="float-sm-end">
                                 <Col className="text-sm-end mt-sm-0 mt-2">
-                                    <Button onClick={() => newTask("Pending", "todoTasks")}>
+
+                                    <Dropdown as={ButtonGroup} align="end">
+                                        <Dropdown.Toggle
+                                            variant="primary"
+                                            id="dropdown-basic"
+                                            className=" p-0"
+                                        >
+                                            <Button className='btn-sm fs-5'>
+                                                <i className="uil-plus me-1"></i>Create
+                                            </Button>
+                                        </Dropdown.Toggle>
+
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item className="notify-item" href="#/">
+                                                <FeatherIcon
+                                                    icon="eye"
+                                                    className="icon-dual icon-xs me-2"
+                                                />
+                                                <span>Show</span>
+                                            </Dropdown.Item>
+                                            <Dropdown.Item className="notify-item" href="#/">
+                                                <FeatherIcon
+                                                    icon="edit"
+                                                    className="icon-dual icon-xs me-2"
+                                                />
+                                                <span>Edit</span>
+                                            </Dropdown.Item>
+                                            <Dropdown.Item className="notify-item" href="#/">
+                                                <FeatherIcon
+                                                    icon="trash-2"
+                                                    className="icon-dual icon-xs me-2"
+                                                />
+                                                <span>Delet</span>
+                                            </Dropdown.Item>
+                                            <Dropdown.Item className="notify-item" href="#/">
+                                                <FeatherIcon
+                                                    icon="filter"
+                                                    className="icon-dual icon-xs me-2"
+                                                />
+                                                <span>Clear group tests</span>
+                                            </Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                    {/* <Button onClick={() => newTask("Pending", "todoTasks")}>
                                         <i className="uil-plus me-1"></i>Create
-                                    </Button>
+                                    </Button> */}
                                 </Col>
                             </div>
                         </Card.Body>
